0b14414130a210bd4e0080aeb44e2c0fa0c5f387
Shawn Thompson <sthompson7720@gmail.com>
2025-07-11 18:19:04 -0400

    Added skeleton game action buttons to the GameBoard component; provided styling for enhanced visuals.

--- frontend/src/components/HomeMenu.jsx
+++ frontend/src/components/HomeMenu.jsx
@@ -1,8 +1,16 @@
 export default function HomeMenu({ onStart }) {
     return (
-        <div className="menu">
-            <p>Welcome to BlackJack! :D</p>
-            <button onClick={onStart}>Start</button>
-        </div>
-    )
+      <div className="flex justify-center items-center flex-col space-y-2 h-screen">
+        <h1 className="text-2xl p-2">Welcome to BlackJack :D</h1>
+        <button
+          className="block py-2 min-w-40 px-4 bg-gray-500 hover:bg-gray-700 hover:text-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md"
+          onClick={onStart}
+        >
+          Start
+        </button>
+        {/* <button className="block py-2 min-w-40 px-4 bg-gray-500 hover:bg-gray-700 hover:text-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-md">
+          Settings
+        </button> */}
+      </div>
+    );
 }
