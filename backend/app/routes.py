a1300f453633efb9f213fcd29e4c08ac66a567e3
Isaac <Scaiaaguinaga@gmail.com>
2025-07-19 16:39:50 -0700

    Added core player object data and hand evaluation

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from app.blackjack.deck import Deck
+from app.blackjack.player import Player
 
 # Blueprint object to organize a group of related routes
 # blueprint name = "game_routes"
@@ -9,6 +10,12 @@ bp = Blueprint("game_routes", __name__)
 # fetching from /api/ will return a json object with an attribute named message
 @bp.route("/")
 def make_deck():
-  test = Deck()
-  test.shuffle()
-  return jsonify(test.deck)
+  deck = Deck()
+  isaac = Player("Isaac")
+  deck.shuffle()
+  isaac.hit(deck.deal_card())
+  isaac.hit(deck.deal_card())
+  isaac.hit(deck.deal_card())
+  print(f"Player hand: {isaac.hand}")
+  print(f"Total: {isaac.evaluate_hand()}")
+  return jsonify(deck.deck)
