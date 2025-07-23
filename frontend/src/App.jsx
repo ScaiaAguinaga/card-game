bfb475ed54121b562f5294e41b7f0c7846355b81
Shawn Thompson <sthompson7720@gmail.com>
2025-07-23 18:24:47 -0400

    Refactored App component by removing unused card state management and temporary card addition logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,29 +3,9 @@ import { useState } from "react";
 import HomeMenu from "./components/HomeMenu";
 import GameBoard from "./components/GameBoard";
 
-import card1 from "./assets/Diamonds/8D.svg";
-import card2 from "./assets/Diamonds/2D.svg";
-
 export default function App() {
   const [screen, setScreen] = useState("home");
-  const [playerCards, setPlayerCards] = useState([]);
-  const [dealerCards, setDealerCards] = useState([]);
 
-  // Take the current (prev) state of a deck and update it with the new card
-  const addPlayerCard = (card) => {
-    setPlayerCards((prev) => [...prev, card]);
-  };
-  const addDealerCard = (card) => {
-    setDealerCards((prev) => [...prev, card]);
-  };
-
-  // Clear the decks
-  const resetGame = () => {
-    setPlayerCards([]);
-    setDealerCards([]);
-  };
-
-  /** Test Buttons */
   const handleHit = () => {
     console.log("Hit button clicked");
   };
@@ -34,17 +14,8 @@ export default function App() {
     console.log("Stand button clicked");
   };
 
-  /**
-   * NOTE: One of the two initial dealer cards MUST be made hidden (flipped) from the user for the game to play out as intended.
-   */
   const handleStartGame = () => {
-    resetGame();
     setScreen("game");
-    /** Manually Add Cards (temporary) */
-    addPlayerCard(card1);
-    addPlayerCard(card2);
-    addDealerCard(card1);
-    addDealerCard(card2);
   };
 
   return (
@@ -53,13 +24,10 @@ export default function App() {
       {screen === "game" && (
         <GameBoard
           onQuit={() => {
-            resetGame();
             setScreen("home");
           }}
           onHit={handleHit}
           onStand={handleStand}
-          playerCards={playerCards}
-          dealerCards={dealerCards}
         />
       )}
     </>
